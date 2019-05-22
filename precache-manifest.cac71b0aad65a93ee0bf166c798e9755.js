393c3779acee0a93b4e27475a36f56b1ad037763
antohepzy <47741661+antohepzy@users.noreply.github.com>
2019-05-22 11:36:26 +0530

    Updates

---
+++ precache-manifest.cac71b0aad65a93ee0bf166c798e9755.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ced69850dd1a4969934d014acaa2ebc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7618b20c5b98dbcc4a40",
+    "url": "/static/css/2.fa8f76ad.chunk.css"
+  },
+  {
+    "revision": "deac8476b295ac75d679",
+    "url": "/static/css/main.507336ad.chunk.css"
+  },
+  {
+    "revision": "7618b20c5b98dbcc4a40",
+    "url": "/static/js/2.835431f8.chunk.js"
+  },
+  {
+    "revision": "deac8476b295ac75d679",
+    "url": "/static/js/main.4a120ec1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
